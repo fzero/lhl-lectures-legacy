6793c8df6c216973ecc36b1c29074ee7f6e970c6
Fabio Neves <fabio@fzero.ca>
2016-02-14 00:57:59 -0500

    Express: added ejs (WIP)

--- expressws/app.js
+++ expressws/app.js
@@ -1,10 +1,29 @@
+// Load Express.js
 var express = require('express');
+var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
 var app = express();
+
+// App middleware
+app.use(serveStatic(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Start web server with the Express app we just created
 var server = require('http').createServer(app);
+
+// Websockets support
 var io = require('socket.io')(server);
 
-app.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/index.html');
+
+app.get('/test', function(req, res) {
+  var data = {
+    name: "Fabio",
+    location: "Lighthouse"
+  }
+  res.render('test', data);
 });
 
 io.on('connection', function(client) {
