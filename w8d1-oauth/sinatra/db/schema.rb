b03ad1d37bc5a04ca9f421b8915a3b189b03a66f
Fabio Neves <fabio@fzero.ca>
2016-02-22 11:37:09 -0500

    Blank db

--- w8d1-oauth/sinatra/db/schema.rb
+++ w8d1-oauth/sinatra/db/schema.rb
@@ -18,9 +18,12 @@ ActiveRecord::Schema.define(version: 20160125184343) do
     t.string   "email"
     t.string   "password"
     t.string   "provider"
+    t.string   "uid"
     t.string   "oauth_token"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "oauth_secret"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
