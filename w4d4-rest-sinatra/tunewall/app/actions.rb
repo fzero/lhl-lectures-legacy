8156c998faf487bdc3e9c6b4aed4d8aab98bd7e6
Fabio Neves <fabio@fzero.ca>
2016-05-18 21:51:29 -0400

    Added before and after comment blocks

--- w4d4-rest-sinatra/tunewall/app/actions.rb
+++ w4d4-rest-sinatra/tunewall/app/actions.rb
@@ -21,6 +21,25 @@ helpers do
 end
 
 
+before do
+  # Stuff that happens before ALL actions go here.
+  # If you want to be specific, make a block with a route
+  # matcher. This is pretty good place control sessions.
+  #
+  # Example:
+  #
+  # before '/protected/*'
+  #   check_login
+  # end
+end
+
+
+after do
+  # Same idea as `before`, but happening AFTER actions.
+  # Potentially less useful, but hey, it's there.
+end
+
+
 # Home
 get '/' do
   if @user
