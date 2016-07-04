5e5556d3ee226ea23a66cded7ebacb3951370d24
Fabio Neves <fabio.fzero@gmail.com>
2016-07-04 18:07:30 -0400

    Code cleanup

--- w6d3-ajax-apis/code/main.js
+++ w6d3-ajax-apis/code/main.js
@@ -28,6 +28,7 @@
     }
   }
 
+
   // Using Handlebars templates:
   // The first line gets the HTML contents of the template...
   var stores_source = $("#stores-template").html()
@@ -38,6 +39,7 @@
   // You can condense the two steps in a single call, of course
   var products_template = Handlebars.compile($("#products-template").html())
 
+
   // The following functions are used with promises on $.ajax calls.
   // Each one receives a single parameter and should return any data
   // objects necessary for the next step.
@@ -46,6 +48,7 @@
     console.log(data)
     return data
   }
+
 
   // This uses returned store data do create an HTML block
   // that we're inserting into the #store-data-container DOM element.
@@ -56,6 +59,7 @@
     return data
   }
 
+
   function displayProductData(data) {
     $('#data-container')
     .empty()
@@ -63,11 +67,21 @@
     return data
   }
 
+
   // LCBO returns prices in cents, so we need to
   // pre-process the data
   function processProductData(data) {
-    data.result.map(function(r) {
+    var results = data.result
+    // Replacing data.result with processed copy created
+    // using Array.prototype.map (think Ruby's Array#map using a
+    // callback instead of a block).
+    data.result = results.map(function(r) {
       r.price = (r.price_in_cents / 100).toFixed(2)
+      // We can add further processing here. For example, I don't
+      // like the "Canada, Region Not Specified" thing on r.origin.
+      // You could use String.prototype.replace() here to fix it.
+      // Another one: some products don't have images. We can test
+      // r.image_thumb_url for null/undefined and add a placeholder.
       return r
     })
     return data
@@ -85,6 +99,7 @@
     .then(logData)
     .then(displayStoreData)
   })
+
 
   $('#product-search').on('submit', function(e) {
     e.preventDefault()
