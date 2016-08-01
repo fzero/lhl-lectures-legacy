4101a7ccba3f9efc95c10ba73a7cecc99aa45417
Fabio Neves <fabio@fzero.ca>
2016-08-01 18:18:44 -0400

    Fixed a stupid bug

--- w3d2-ORM-Intro/code/justpg.rb
+++ w3d2-ORM-Intro/code/justpg.rb
@@ -16,6 +16,6 @@ end
 puts "Which ID do you want? "
 id = gets.chomp
 
-DB.exec_params("SELECT * FROM countries WHERE id=$1 AND name=$2;", [id, name]) do |results|
+DB.exec_params("SELECT * FROM countries WHERE id=$1;", [id]) do |results|
   p results[0]
 end
