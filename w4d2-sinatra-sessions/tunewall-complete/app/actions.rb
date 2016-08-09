d412a6a4da10d033939f8630f7615d000a120433
Fabio Neves <fabio@fzero.ca>
2016-08-09 11:27:18 -0400

    Commented actions.rb a bit better

--- w4d2-sinatra-sessions/tunewall-complete/app/actions.rb
+++ w4d2-sinatra-sessions/tunewall-complete/app/actions.rb
@@ -1,15 +1,9 @@
-helpers do
-
-  def check_user
-    @user = User.find_by(id: session[:user_id])
-    if @user
-      session.delete(:login_error)
-    else
-      session[:login_error] = "You must be logged in."
-      redirect '/login'
-    end
+def check_login
+  @user = User.find_by(id: session[:user_id])
+  unless @user
+    session[:login_error] = "You must be logged in"
+    redirect '/login'
   end
-
 end
 
 # Home
@@ -19,6 +13,8 @@ end
 
 # List cookies
 get '/cookies' do
+  # Here we set two cookies. They will still show up in the cookies List
+  # if we comment out the lines below after going to this action.
   cookies[:cookie1] = "Chocolate chip"
   cookies[:cookie2] = "Oreo"
   erb :cookies
@@ -26,6 +22,8 @@ end
 
 # List sessions
 get '/sessions' do
+  # Here we set two sessions. They will still show up in the sessions List
+  # if we comment out the lines below after going to this action.
   session[:secret1] = "This is a secret session variable!"
   session[:secret2] = "And this is another one"
   erb :sessions
@@ -42,12 +40,18 @@ end
 post '/validate' do
   email = params[:email]
   password = params[:password]
+  # We're using has_secure_password on User, so we'll check the password using
+  # User#authenticate. Now, because we can get a wrong email too, we can have
+  # a situation where User.find_by(email ...) will return nil, blowing up
+  # on authenticate. To avoid that we'll use the .try() method, which
+  # automatically stops if we get a nil from User#find_by.
   user = User.find_by(email: email).try(:authenticate, password)
   if user
+    session.delete(:login_error) # Login successful, delete login error message
     session[:user_id] = user.id
     redirect '/tunes'
   else
-    session.delete(:user_id)
+    session.delete(:user_id) # Just to make sure we're logged out
     session[:login_error] = "You must be logged in."
     redirect '/login'
   end
